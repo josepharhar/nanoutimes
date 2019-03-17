a4a30074a79b4daac5834faa471f2b99113b70f5
Joseph Arhar <josepharhar@gmail.com>
2019-03-17 08:15:01 +0000

    mac does have somthing, starting work again

--- nanoutimes.cc
+++ nanoutimes.cc
@@ -3,6 +3,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <sys/stat.h>
+#include <string.h>
 
 #define NEW_STRING(str) \
   v8::String::NewFromUtf8(isolate, str, v8::NewStringType::kNormal) \
@@ -27,39 +28,43 @@ static void utimesSync(
   v8::String::Utf8Value utf8_filepath(isolate, filepath);
   char* char_filepath = *utf8_filepath;
 
-  if (!args[1]->IsNumber()) {
-    // TODO
-    args.GetReturnValue().Set(NEW_STRING("atimeS must be a number"));
-    return;
-  }
-  uint64_t atimeS = (uint64_t)(v8::Local<v8::Number>::Cast(args[1])->Value());
+  // If the input numbers are less than zero, then don't change the time.
 
-  if (!args[2]->IsNumber()) {
+  if (!args[1]->IsBigIntObject()) {
     // TODO
-    args.GetReturnValue().Set(NEW_STRING("atimeNs must be a number"));
+    args.GetReturnValue().Set(NEW_STRING("atimeS must be a BigInt"));
     return;
   }
-  uint64_t atimeNs = (uint64_t)(v8::Local<v8::Number>::Cast(args[2])->Value());
+  uint64_t atimeS = (uint64_t)(v8::Local<v8::BigInt>::Cast(args[1])->Value());
 
-  if (!args[3]->IsNumber()) {
+  if (!args[2]->IsBigIntObject()) {
     // TODO
-    args.GetReturnValue().Set(NEW_STRING("mtimeS must be a number"));
+    args.GetReturnValue().Set(NEW_STRING("atimeNs must be a BigInt"));
     return;
   }
-  uint64_t mtimeS = (uint64_t)(v8::Local<v8::Number>::Cast(args[3])->Value());
+  uint64_t atimeNs = (uint64_t)(v8::Local<v8::BigInt>::Cast(args[2])->Value());
 
-  if (!args[4]->IsNumber()) {
+  if (!args[3]->IsBigIntObject()) {
     // TODO
-    args.GetReturnValue().Set(NEW_STRING("mtimeNs must be a number"));
+    args.GetReturnValue().Set(NEW_STRING("mtimeS must be a BigInt"));
     return;
   }
-  uint64_t mtimeNs = (uint64_t)(v8::Local<v8::Number>::Cast(args[4])->Value());
+  uint64_t mtimeS = (uint64_t)(v8::Local<v8::BigInt>::Cast(args[3])->Value());
+
+  if (!args[4]->IsBigIntObject()) {
+    // TODO
+    args.GetReturnValue().Set(NEW_STRING("mtimeNs must be a BigInt"));
+    return;
+  }
+  uint64_t mtimeNs = (uint64_t)(v8::Local<v8::BigInt>::Cast(args[4])->Value());
 
 #ifdef _WIN32
   // TODO
 #else  // _WIN32
-  struct timeval times[2];
-  memset(times, 0, sizeof(struct timeval) * 2);
+  struct timespec times[2];
+  memset(times, 0, sizeof(struct timespec) * 2);
+
+  if (atimeS < 0 || atimeNs < 0) 
   times[0].tv_sec = atimeS;
   times[0].tv_nsec = atimeNs;
   times[1].tv_sec = mtimeS;
